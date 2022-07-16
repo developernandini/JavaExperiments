ae5137d0f915e123c0309af41b2da554743e1be1
Nandini <oberoinandini19@gmail.com>
2022-07-16 15:19:08 +0530

    updated program file

--- prorgam.java
+++ prorgam.java
@@ -12,7 +12,7 @@ public class prorgam {
          
         System.out.println("Array After Bubble Sort");  
         for(int i=0; i < arr.length; i++){  
-                System.out.print(arr[i] + " ");  
+                System.out.print(arr[i] + "-");  
         }  
 
 }  
